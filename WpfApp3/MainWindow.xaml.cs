9130edcbd59b8cc56e34d04e3162aaddc29a5a92
user <usr123@gmail.com>
2024-06-04 09:39:36 +0300

    Созданный проект

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace WpfApp3;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnGen_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (!File.Exists("List.txt"))
+            return;
+        var text = File.ReadAllText("List.txt");
+        
+        var listStudents = text.Split('\n');
+        
+        Random rnd = new Random();
+        
+        int index = rnd.Next(0, listStudents.Length);
+        
+        string currentFio = listStudents[index];
+        
+        if (!StudentsList.Items.Contains(currentFio))
+            StudentsList.Items.Add(currentFio);
+    }
+
+    private void BtnTrash_OnClick(object sender, RoutedEventArgs e)
+    {
+        
+    }
+}
